dc71134ce32d48a2af9526b44bf440c8709620a9
x10s <x10s@outlook.com>
2014-01-29 21:45:56 -0500

    Added module for sampling using the M&M algo

--- dbpsk.m
+++ dbpsk.m
@@ -10,10 +10,10 @@ d_phase = 0;
 derotatedRealInput = 100 : 1; %This creates an empty array
 derotatedImaginaryInput = 100 : 1; %This creates an empty array
 %Open input file 
-fid = fopen('testdata', 'r');
-while ~feof(fid)
+testdata = fopen('testdata', 'r');
+while ~feof(testdata)
     %Read each sample
-    row = fscanf(fid, '%f %f', [2 1]);
+    row = fscanf(testdata, '%f %f', [2 1]);
     
     if ~isempty(row)
         %Rotate the sample about the origin
@@ -33,6 +33,81 @@ while ~feof(fid)
 end
 
 %Open costasoutR for writing  
-fid1 = fopen('costasoutR', 'w');
-fprintf(fid1,'%f %f',derotatedRealInput);
-fclose(fid1);
+costasoutR_write = fopen('costasoutR', 'w');
+fprintf(costasoutR_write,'%f \n',derotatedRealInput);
+fclose(costasoutR_write);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%Module 2%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%Taking the samples%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+timingError = 0;
+decision_plus_one = 0;
+decision_minus_one = 0;
+sample_plus_one = 0;
+sample_minus_one = 0;
+counter = 0;
+readPosition = 10;
+sampleList = 100 : 1; %This creates an empty array
+sampleListActual = 100 : 1; %This creates an empty array
+sampleCounter = 1;
+%Open input file 
+costasoutR_read = fopen('costasoutR', 'r');
+while ~feof(costasoutR_read)
+   %Read each sample
+   sample = fscanf(costasoutR_read, '%f', [1,1]);
+   %Read first three samples to get started
+   if(counter < 3)
+       if(counter == 0)
+           sample_minus_one = sample(1,1);
+           if sample_minus_one > 0
+               decision_minus_one = 1;
+           else 
+               decision_minus_one = 0;
+           end
+           sampleList(sampleCounter) = decision_minus_one;
+       elseif(counter == 1)
+           if sample(1,1) > 0
+               decision = 1;
+           else 
+               decision = 0;
+           end
+           sampleList(sampleCounter) = decision;
+       elseif(counter == 2)
+           sample_plus_one = sample(1,1);
+           if sample_plus_one > 0
+               decision_plus_one = 1;
+           else 
+               decision_plus_one = 0;
+           end
+           sampleList(sampleCounter) = decision_plus_one;
+           %Calculate the timing error based on the M&M algorithm
+           timingError = floor((decision_minus_one - decision_plus_one)*sample + (sample_plus_one - sample_minus_one)*decision);
+           %Samples are taken every 10 microseconds
+           readPosition = readPosition + 10 + timingError;
+       end
+       sampleCounter = sampleCounter + 1;
+   end
+   %Now continue with the rest of the samples
+   if(counter == readPosition)
+       %Make a decision on the bit value based on whether its negative or
+       %positive
+       if sample(1,1) > 0
+           decision = 1;
+       else 
+           decision = 0;
+       end
+       sampleList(sampleCounter) = decision;
+       sampleCounter = sampleCounter + 1;
+       %Calculate the timing error based on the M&M algorithm
+       timingError = floor((decision_minus_one - decision_plus_one)*sample(1,1) + (sample_plus_one - sample_minus_one)*decision);
+       %Samples are taken every 10 microseconds
+       readPosition = readPosition + 10 + timingError;
+   end
+   counter = counter + 1;
+end
+
+%Open mmdecisions for writing  
+mmdecisions_write = fopen('mmdecisions', 'w');
+fprintf(mmdecisions_write,'%d',sampleList);
+fclose(mmdecisions_write);
